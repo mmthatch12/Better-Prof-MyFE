aee2b9e81dfa72ee229dfdf528cdb13f0c4bfccc
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-01 09:48:27 -0600

    working on StudentList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom'
 
 import Login from './components/Register & Login/Login'
 import Register from './components/Register & Login/Register'
+import StudentList from './components/Professor/StudentList'
 
 function App() {
+  const [list, setList] = useState([])
+
   return (
     <div className="App">
       <Route exact path='/' component={Login} />
       <Route path='/register' component={Register} />
+      <Route path='/studentlist/user/:id' render={props =>
+        <StudentList {...props} list={list} setList={setList} />}
+      />
     </div>
   );
 }
---
+++ src/components/Professor/StudentList.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import AxiosWithAuth from '../../utils/AxiosWithAuth'
+
+const StudentList = ({ list, setList }) => {
+  const userId = localStorage.getItem(id)
+  
+  useEffect(() => {
+      AxiosWithAuth().get(`https://better-professor-backend.herokuapp.com/students/user/${userId}`)
+        .then(res => {
+            console.log(res.data)
+            setList(res.data)
+        })
+  },[])
+
+  console.log(list)
+  
+  return (
+      <h1>From StudentList</h1>
+  )
+} 
+
+export default StudentList
