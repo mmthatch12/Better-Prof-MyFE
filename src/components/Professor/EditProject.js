a20fa8b7c42d4038ec30d2097e959b60103af987
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-05 20:57:34 -0600

    working on EditProject component

--- src/components/Professor/EditProject.js
+++ src/components/Professor/EditProject.js
@@ -1,6 +1,44 @@
 import React, { useState, useEffect } from 'react'
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const EditProject = (props) => {
+    const classes = useStyles()
+    const [eProject, setEProject] = useState([])
+
     return (
         <h1>From EditProject</h1>
     )
