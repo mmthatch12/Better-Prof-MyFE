b3370e9148e39c08267a1c7524e2a25a864848d4
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-08 11:30:32 -0600

    fixed addstudent link and deletelink

--- src/components/Navs/StudentListNav.js
+++ src/components/Navs/StudentListNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -17,6 +18,8 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import AddIcon from '@material-ui/icons/Add';
+import TransitEnterexitIcon from '@material-ui/icons/TransitEnterexit';
 
 const drawerWidth = 240;
 
@@ -76,7 +79,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function StudentListNav() {
+export default function StudentListNav(props) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -138,18 +141,9 @@ export default function StudentListNav() {
         </div>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Add Student', 'Logout'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemIcon>{index % 2 === 0 ? <Link to='/studentlist/addstudent'><AddIcon /></Link> : <Link to='/' onClick={() => localStorage.clear()}><TransitEnterexitIcon /></Link>}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
